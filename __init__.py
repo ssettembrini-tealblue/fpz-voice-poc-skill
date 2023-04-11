9319783422011ad3da0a8392c7dd621741bcc890
DESKTOP-O8E6P92\stefa <stefano.settembrini@teal.blue>
2023-04-11 16:51:14 +0200

    added basic intent, dialog

--- __init__.py
+++ __init__.py
@@ -1,15 +1,17 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import json
 
 class FpzVoicePoc(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('poc.voice.fpz.intent')
+    @intent_file_handler('SetFrequency.intent')
     def handle_poc_voice_fpz(self, message):
-        self.speak_dialog('poc.voice.fpz')
-
+        freq_offset = message.data['freq']
+        self.speak_dialog('IncreasedFrequency', {
+                'offset': freq_offset})
+        time.sleep(1)
 
 def create_skill():
-    return FpzVoicePoc()
+    return FpzVoicePoc() *
 
