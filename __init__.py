01ecf3ad22687361be2cb5531fa1653a7b7e5305
DESKTOP-O8E6P92\stefa <stefano.settembrini@teal.blue>
2023-04-11 17:24:04 +0200

    added extract number

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from mycroft import MycroftSkill, intent_file_handler
 import json
+from mycroft.util.parse import extract_number
 
 class FpzVoicePoc(MycroftSkill):
     def __init__(self):
@@ -7,7 +8,7 @@ class FpzVoicePoc(MycroftSkill):
 
     @intent_file_handler('SetFrequency.intent')
     def set_frequency(self, message):
-        freq_offset = message.data['freq']
+        freq_offset = int(extract_number(message.data['freq']))
         self.speak_dialog('IncreasedFrequency', {
                 'offset': freq_offset})
         time.sleep(1)
