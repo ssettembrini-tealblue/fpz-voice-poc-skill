1a9e193ec2be26376cbbf62fa73e23056247b1be
DESKTOP-O8E6P92\stefa <stefano.settembrini@teal.blue>
2023-04-13 10:49:02 +0200

    fixed intent, script

--- __init__.py
+++ __init__.py
@@ -7,18 +7,18 @@ class FpzVoicePoc(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    # @intent_file_handler('SetFrequency.intent')
-    # def set_frequency(self, message):
-        # self.speak_dialog('OperationDone')
-        # freq_offset = int(extract_number(message.data.get('freq')))
-        # self.speak_dialog('IncreasedFrequency', {
-                # 'offset': freq_offset})
-        #time.sleep(1)
+    @intent_file_handler('SetFrequency.intent')
+    def set_frequency(self, message):
+        self.speak_dialog('OperationDone')
+        freq_offset = int(extract_number(message.data.get('freq')))
+        self.speak_dialog('IncreasedFrequency', {
+                'offset': freq_offset})
+        time.sleep(1)
         
     @intent_file_handler('poc.voice.fpz.intent')
     def handle_poc_voice_fpz(self, message):
         self.speak_dialog('poc.voice.fpz')
         
 def create_skill():
-    return FpzVoicePoc() *
+    return FpzVoicePoc()
 
