588bfdca02e1acbe3fbcdf3509b2536436d881e8
DESKTOP-O8E6P92\stefa <stefano.settembrini@teal.blue>
2023-04-13 14:37:38 +0200

    added msgbus for all functions

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ from mycroft.messagebus import Message
 
 
 def send_bus(self,type_function, value):  
-    self.bus.emit(Message("recognizer_loop:" + type_function,
+    self.bus.emit(Message(type_function,
                           {'utterances': [value],  
                             'lang': 'en-us'}))
 
@@ -19,7 +19,7 @@ class FpzVoicePoc(MycroftSkill):
         self.speak_dialog('OperationDone')
         targetvalue = int(extract_number(message.data.get('freq')))
         self.speak_dialog('ChangedFrequency', {'value': targetvalue})
-        send_bus(self,"Increase",targetvalue);
+        send_bus(self,"set",targetvalue);
     
     @intent_file_handler('IncreaseFrequency.intent')
     def handle_increase_frequency(self, message):
@@ -27,23 +27,27 @@ class FpzVoicePoc(MycroftSkill):
         offsetincrease = int(extract_number(message.data.get('freq')))
         self.speak_dialog('IncreasedFrequency', 
                           {'offsetincrease': offsetincrease})
+        send_bus(self,"increase",offsetincrease);
     
     @intent_file_handler('DecreaseFrequency.intent')
     def handle_decrease_frequency(self, message):
         self.speak_dialog('OperationDone')
         offsetvalue = int(extract_number(message.data.get('freq')))
         self.speak_dialog('DecreasedFrequency', {'offset': offsetvalue})
+        send_bus(self,"decrease",offsetvalue);
     
     @intent_file_handler('StartBlower.intent')
     def handle_start_blower(self, message):
         self.speak_dialog('OperationDone')       
         self.speak_dialog('StartedBlower')
+        send_bus(self,"start",1);
 
     
     @intent_file_handler('StopBlower.intent')
     def handle_stop_blower(self, message):
         self.speak_dialog('OperationDone')       
         self.speak_dialog('StoppedBlower')
+        send_bus(self,"stop",0);
     
 def create_skill():
     return FpzVoicePoc()
