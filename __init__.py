1fcd5db0be2ccc8c8c32635cebf532070379812e
DESKTOP-O8E6P92\stefa <stefano.settembrini@teal.blue>
2023-04-13 14:25:20 +0200

    added msgbus function

--- __init__.py
+++ __init__.py
@@ -2,16 +2,25 @@ from mycroft import MycroftSkill, intent_file_handler
 import json
 import time
 from mycroft.util.parse import extract_number
+from mycroft.messagebus import Message
+
+
+def send_bus(self,type_function, value):  
+    self.bus.emit(Message(type_function,  
+                          {'utterances': [value],  
+                            'lang': 'en-us'}))  
 
 class FpzVoicePoc(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+
     
     @intent_file_handler('SetFrequency.intent')
     def set_frequency(self, message):
         self.speak_dialog('OperationDone')
         targetvalue = int(extract_number(message.data.get('freq')))
         self.speak_dialog('ChangedFrequency', {'value': targetvalue})
+        send_bus(self,"Increase",targetvalue);
     
     @intent_file_handler('IncreaseFrequency.intent')
     def handle_increase_frequency(self, message):
