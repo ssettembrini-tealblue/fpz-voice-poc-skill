a7e0e4d10387cc7f742c433c75d9cd6e15b10838
DESKTOP-O8E6P92\stefa <stefano.settembrini@teal.blue>
2023-04-13 14:05:13 +0200

    removed sleep

--- __init__.py
+++ __init__.py
@@ -12,33 +12,29 @@ class FpzVoicePoc(MycroftSkill):
         self.speak_dialog('OperationDone')
         targetvalue = int(extract_number(message.data.get('freq')))
         self.speak_dialog('ChangedFrequency', {'value': targetvalue})
-        time.sleep(1)
     
     @intent_file_handler('IncreaseFrequency.intent')
     def set_frequency(self, message):
         self.speak_dialog('OperationDone')
         offsetincrease = int(extract_number(message.data.get('freq')))
         self.speak_dialog('IncreasedFrequency', {'offsetincrease': offsetincrease})
-        time.sleep(1)
     
     @intent_file_handler('DecreaseFrequency.intent')
     def set_frequency(self, message):
         self.speak_dialog('OperationDone')
         offsetvalue = int(extract_number(message.data.get('freq')))
         self.speak_dialog('DecreasedFrequency', {'offset': offsetvalue})
-        time.sleep(1)
     
     @intent_file_handler('StartBlower.intent')
     def set_frequency(self, message):
         self.speak_dialog('OperationDone')       
         self.speak_dialog('StartedBlower')
-        time.sleep(1)
+
     
     @intent_file_handler('StopBlower.intent')
     def set_frequency(self, message):
         self.speak_dialog('OperationDone')       
         self.speak_dialog('StoppedBlower')
-        time.sleep(1)
     
 def create_skill():
     return FpzVoicePoc()
