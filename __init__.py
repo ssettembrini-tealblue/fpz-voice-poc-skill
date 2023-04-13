b99c7ae89edc5d0a2e4a38dec6f5cf73f997f65c
DESKTOP-O8E6P92\stefa <stefano.settembrini@teal.blue>
2023-04-13 11:55:19 +0200

    fixed indentation

--- __init__.py
+++ __init__.py
@@ -6,32 +6,28 @@ from mycroft.util.parse import extract_number
 class FpzVoicePoc(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
-
+    
     @intent_file_handler('SetFrequency.intent')
     def set_frequency(self, message):
         self.speak_dialog('OperationDone')
         targetvalue = int(extract_number(message.data.get('freq')))
-        self.speak_dialog('ChangedFrequency', {
-                'value': targetvalue})
+        self.speak_dialog('ChangedFrequency', {'value': targetvalue})
         time.sleep(1)
     
     @intent_file_handler('IncreaseFrequency.intent')
     def set_frequency(self, message):
         self.speak_dialog('OperationDone')
         offsetvalue = int(extract_number(message.data.get('freq')))
-        self.speak_dialog('IncreasedFrequency', {
-                'offset': offsetvalue})
+        self.speak_dialog('IncreasedFrequency', {'offset': offsetvalue})
         time.sleep(1)
     
     @intent_file_handler('DecreaseFrequency.intent')
     def set_frequency(self, message):
         self.speak_dialog('OperationDone')
         offsetvalue = int(extract_number(message.data.get('freq')))
-        self.speak_dialog('DecreasedFrequency', {
-                'offset': offsetvalue})
+        self.speak_dialog('DecreasedFrequency', {'offset': offsetvalue})
         time.sleep(1)
     
-        
 def create_skill():
     return FpzVoicePoc()
 
