ff08e76878549c45a226a7cfca6679773db9b244
DESKTOP-O8E6P92\stefa <stefano.settembrini@teal.blue>
2023-06-14 15:46:13 +0200

    fixed errors on non numeric values

--- __init__.py
+++ __init__.py
@@ -26,23 +26,37 @@ class FpzVoicePoc(MycroftSkill):
         
     @intent_file_handler('SetFrequency.intent')
     def set_frequency(self, message):
-        self.speak_dialog('OperationDone')
+        isfreqdigit = extract_number(message.data.get('freq'))
+        if isfreqdigit==False:
+          self.speak_dialog('ErrorOperation')
+          return
         targetvalue = int(extract_number(message.data.get('freq')))
+        self.speak_dialog('OperationDone')
         self.speak_dialog('ChangedFrequency', {'value': targetvalue})
         send_bus(self,"set",targetvalue);
+        
+     
     
     @intent_file_handler('IncreaseFrequency.intent')
     def handle_increase_frequency(self, message):
-        self.speak_dialog('OperationDone')
+        isfreqdigit = extract_number(message.data.get('freq'))
+        if isfreqdigit==False:
+          self.speak_dialog('ErrorOperation')
+          return
         offsetincrease = int(extract_number(message.data.get('freq')))
+        self.speak_dialog('OperationDone')
         self.speak_dialog('IncreasedFrequency', 
                           {'offsetincrease': offsetincrease})
         send_bus(self,"increase",offsetincrease);
     
     @intent_file_handler('DecreaseFrequency.intent')
     def handle_decrease_frequency(self, message):
-        self.speak_dialog('OperationDone')
+        isfreqdigit = extract_number(message.data.get('freq'))
+        if isfreqdigit==False:
+          self.speak_dialog('ErrorOperation')
+          return
         offsetvalue = int(extract_number(message.data.get('freq')))
+        self.speak_dialog('OperationDone')
         self.speak_dialog('DecreasedFrequency', {'offset': offsetvalue})
         send_bus(self,"decrease",offsetvalue);
     
